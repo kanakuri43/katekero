7d3905d0924666e7f4216e1f78324e5978c115f5
okita <junor01@gmail.com>
2025-02-02 16:34:17 +0900

    kintoneからダウンロード

--- katekero/ViewModels/DashboardViewModel.cs
+++ katekero/ViewModels/DashboardViewModel.cs
@@ -7,6 +7,10 @@ using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
+using System.Net.Http.Headers;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
 
 namespace katekero.ViewModels
 {
@@ -16,6 +20,8 @@ namespace katekero.ViewModels
         private ObservableCollection<Sale> _sales;
         private int _selectedSaleNo;
         private DateTime _selectedDate;
+        private string _json;
+        private DateTime _lastFetchecAt;
 
         public ObservableCollection<Sale> Sales
         {
@@ -32,6 +38,16 @@ namespace katekero.ViewModels
             get { return _selectedDate; }
             set { SetProperty(ref _selectedDate, value); }
         }
+        public string Json
+        {
+            get { return _json; }
+            set { SetProperty(ref _json, value); }
+        }
+        public DateTime LastFetchedAt
+        {
+            get { return _lastFetchecAt; }
+            set { SetProperty(ref _lastFetchecAt, value); }
+        }
 
         public DashboardViewModel(IRegionManager regionManager)
         {
@@ -41,6 +57,7 @@ namespace katekero.ViewModels
             RegisterCommand = new DelegateCommand(Register);
             ForwardCommand = new DelegateCommand(Forward);
             BackwardCommand = new DelegateCommand(Backward);
+            ReloadCommand = new DelegateCommand(Reload);
 
             using (var context = new AppDbContext())
             {
@@ -56,6 +73,7 @@ namespace katekero.ViewModels
         public DelegateCommand SelectedDateChangedCommand { get; }
         public DelegateCommand ForwardCommand { get; }
         public DelegateCommand BackwardCommand { get; }
+        public DelegateCommand ReloadCommand { get; }
 
         private void Register()
         {
@@ -74,6 +92,35 @@ namespace katekero.ViewModels
             this.SelectedDate = this.SelectedDate.AddDays(-1);
             ShowSalesList();
         }
+        private async void Reload()
+        {
+            await FetchKintone(new string[] { });
+
+            LastFetchedAt = DateTime.Now;
+        }
+
+        private async Task FetchKintone(string[] args)
+        {
+            HttpClient client = new HttpClient();
+            string apiToken = "ZQjrR6cwgLLBpt5NZQU4INEg8pAeWyE0AYdSWEHg"; // kintoneのAPIトークンを入力
+
+            // ヘッダーにAPIトークンを設定
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            client.DefaultRequestHeaders.Add("X-Cybozu-API-Token", apiToken);
+
+            // レコード取得のURL
+            string url = $"https://vk5k755s9nir.cybozu.com/k/v1/records.json?app=140";
+
+            // レコードを取得
+            HttpResponseMessage response = await client.GetAsync(url);
+
+            string responseBody = await response.Content.ReadAsStringAsync();
+
+            // Jsonプロパティにパースした結果を設定
+            Json = JObject.Parse(responseBody).ToString();
+        }
+
 
         private void SaleDoubleClick()
         {
