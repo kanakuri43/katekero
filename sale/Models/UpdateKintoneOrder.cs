0a528371efb19cec40992e97addbad6d5b5a5457
okita <t-okita@netoffice.obisan.co.jp>
2025-02-21 14:53:59 +0900

    update

--- sale/Models/UpdateKintoneOrder.cs
+++ sale/Models/UpdateKintoneOrder.cs
@@ -19,7 +19,6 @@ namespace sale.Models
 
             return new
             {
-                //ConnectionString = doc.Root.Element("Database").Element("ConnectionString").Value.Trim(),
                 BaseUrl = doc.Root.Element("Kintone").Element("BaseUrl").Value.Trim(),
                 AppId = doc.Root.Element("Kintone").Element("AppId").Value.Trim(),
                 ApiToken = doc.Root.Element("Kintone").Element("ApiToken").Value.Trim(),
@@ -40,70 +39,58 @@ namespace sale.Models
 
         public static async void Update(int OrderNo)
         {
-            // サンプルデータ
-            var fetchedData = new List<(string 年月, string 担当者, double 売上実績, double 粗利実績)>
+            string query = $"order_no = \"{OrderNo}\" ";
+
+            var getParams = new Dictionary<string, string>
             {
-                ("202410", "s-motoki@netoffice.obisan.co.jp",  10, 10),
-                //("202410", "m-sugawara@netoffice.obisan.co.jp", 20, 20)
+                { "app", _config.AppId },
+                { "query", query }
             };
 
-            // 
-            //foreach (var d in fetchedData)
-            //{
-                string query = $"order_no = \"{OrderNo}\" ";
+            using (HttpClient client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Add("X-Cybozu-API-Token", _config.ApiToken);
+                client.DefaultRequestHeaders.Add("Authorization", "Basic " + _config.BasicPassword);
 
-                var getParams = new Dictionary<string, string>
+                var response = await client.GetAsync(_config.BaseUrl + "records.json?" + UrlEncode(getParams));
+                if (!response.IsSuccessStatusCode)
                 {
-                    { "app", _config.AppId },
-                    { "query", query }
-                };
+                    Console.WriteLine("Fetch Kintone Error");
+                    return;
+                }
 
-                using (HttpClient client = new HttpClient())
+                var responseData = JsonConvert.DeserializeObject<Dictionary<string, object>>(await response.Content.ReadAsStringAsync());
+                var recordsArray = (Newtonsoft.Json.Linq.JArray)responseData["records"];
+                var records = recordsArray.ToObject<List<object>>();
+
+                if (records.Count > 0)
                 {
-                    client.DefaultRequestHeaders.Add("X-Cybozu-API-Token", _config.ApiToken);
-                    client.DefaultRequestHeaders.Add("Authorization", "Basic " + _config.BasicPassword);
-
-                    var response = await client.GetAsync(_config.BaseUrl + "records.json?" + UrlEncode(getParams));
-                    if (!response.IsSuccessStatusCode)
+                    foreach (var record in records)
                     {
-                        Console.WriteLine("Fetch Kintone Error");
-                        return;
-                    }
+                        var recordDict = ((Newtonsoft.Json.Linq.JObject)record).ToObject<Dictionary<string, object>>();
+                        string recordId = OrderNo.ToString();
 
-                    var responseData = JsonConvert.DeserializeObject<Dictionary<string, object>>(await response.Content.ReadAsStringAsync());
-                    var recordsArray = (Newtonsoft.Json.Linq.JArray)responseData["records"];
-                    var records = recordsArray.ToObject<List<object>>();
-
-                    if (records.Count > 0)
-                    {
-                        foreach (var record in records)
+                        var postParams = new Dictionary<string, object>
                         {
-                            var recordDict = ((Newtonsoft.Json.Linq.JObject)record).ToObject<Dictionary<string, object>>();
-                            //string recordId = ((Newtonsoft.Json.Linq.JObject)recordDict["$id"])["value"].ToString();
-                            string recordId = OrderNo.ToString();
-
-                            var postParams = new Dictionary<string, object>
-                            {
-                                { "app", _config.AppId },
-                                { "id", recordId },
-                                { "record", new Dictionary<string, object>
-                                    {
-                                        { "sold", new Dictionary<string, object> { { "value", 1 } } }   // 売上済みを1に更新
-                                    }
+                            { "app", _config.AppId },
+                            { "id", recordId },
+                            { "record", new Dictionary<string, object>
+                                {
+                                    { "sold", new Dictionary<string, object> { { "value", 1 } } }   // 売上済みを1に更新
                                 }
-                            };
-
-                            var postContent = new StringContent(JsonConvert.SerializeObject(postParams), Encoding.UTF8, "application/json");
-                            var postResponse = await client.PutAsync(_config.BaseUrl + "record.json", postContent);
-
-                            if (!postResponse.IsSuccessStatusCode)
-                            {
-                                Console.WriteLine("Register Kintone Error" + postResponse.StatusCode);
                             }
+                        };
 
+                        var postContent = new StringContent(JsonConvert.SerializeObject(postParams), Encoding.UTF8, "application/json");
+                        var postResponse = await client.PutAsync(_config.BaseUrl + "record.json", postContent);
+
+                        if (!postResponse.IsSuccessStatusCode)
+                        {
+                            Console.WriteLine("Register Kintone Error" + postResponse.StatusCode);
                         }
+
                     }
-                //}
+                }
             }
         }
     }
